3e24953f7806f0e8bf89ffa2a7add293bb0bfa73
colinslda <colin.soldati@icloud.com>
2025-02-06 14:30:00 +0100

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+    "name": "Compresseur Nuance",
+    "short_name": "Compresseur",
+    "icons": [
+        {
+            "src": "images/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "images/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ],
+    "theme_color": "#301934",
+    "background_color": "#001F3F",
+    "display": "standalone",
+    "start_url": ".",
+    "scope": "."
+}
